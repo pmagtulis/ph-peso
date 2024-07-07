de5980fd20a00ce160ab8da0d53200d019fad1ab
Prinz Magtulis <ppm2130@columbia.edu>
2024-07-06 22:02:28 -0400

    revised

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
 gsap.registerPlugin(ScrollTrigger);
 
-const image = document.querySelector(".massiveImage");
+const container = document.querySelector('.container');
 
-gsap.to(image, {
-  x: () => - (image.clientWidth - window.innerWidth),
-  ease: "none",
-  scrollTrigger: {
-    trigger: image,
-    start: "top top",
-    end: () => (image.clientWidth - window.innerWidth),
-    scrub: true,
-    pin: true,
-    invalidateOnRefresh: true,
-    anticipatePin: 1,
-    markers: true,
-  }
+gsap.to(container, {
+    x: () => -(container.scrollWidth - document.documentElement.clientWidth),
+    ease: "none",
+    scrollTrigger: {
+        trigger: container,
+        pin: true,
+        scrub: 1,
+        end: () => "+=" + (container.scrollWidth - document.documentElement.clientWidth),
+        invalidateOnRefresh: true
+    }
 });
